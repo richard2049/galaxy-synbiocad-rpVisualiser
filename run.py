c200f0354aeb239e83c7c987f80b9a9c7dd2a9e7
mdulac <melchior.dulac@inra.fr>
2020-07-09 17:56:03 +0200

    updated with the last version

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ def main(inputTar,
          input_format,
          output):
     docker_client = docker.from_env()
-    image_str = 'brsynth/rpvisualiser-standalone:survey'
+    image_str = 'brsynth/rpvisualiser-standalone:dev'
     try:
         image = docker_client.images.get(image_str)
     except docker.errors.ImageNotFound:
