deb5dc226f10a0b3ee038dc9eade4c4f727b036f
mdulac <melchior.dulac@inra.fr>
2020-11-24 17:00:12 +0100

    removed the docker tags

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM brsynth/rpbase:v2
+FROM brsynth/rpbase
 
 WORKDIR /home/
 
--- run.py
+++ run.py
@@ -34,7 +34,7 @@ def main(inputfile,
     :return: None
     """
     docker_client = docker.from_env()
-    image_str = 'brsynth/rpvisualiser-standalone:v2'
+    image_str = 'brsynth/rpvisualiser-standalone'
     try:
         image = docker_client.images.get(image_str)
     except docker.errors.ImageNotFound:
